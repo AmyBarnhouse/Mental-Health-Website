85ca62657ddef6562cc57ccc8c9175d9f543b515
AmyBarnhouse <107323584+AmyBarnhouse@users.noreply.github.com>
2022-11-18 14:49:45 +0000

    added js file and thank you response to user after submit button

---
+++ javaScript.js
@@ -0,0 +1,6 @@
+document.getElementById("submitBtn").addEventListener("click", thankYouMsg);
+
+function thankYouMsg() {
+  document.getElementById("tests").innerHTML = "Thank you for signing up! :)";
+}
+
