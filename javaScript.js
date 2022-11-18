3c739d1e22d86671e24da79602d208ab0ad1cbb8
AmyBarnhouse <107323584+AmyBarnhouse@users.noreply.github.com>
2022-11-18 15:20:20 +0000

    new button added for user to ask questions

--- javaScript.js
+++ javaScript.js
@@ -1,15 +1,21 @@
+
+/* Displays thank you message after user submits form */ 
 document.getElementById("submitBtn").addEventListener("click", thankYouMsg);
 
 function thankYouMsg() {
   document.getElementById("tests").innerHTML = "Thank you for signing up! :)";
 }
 
-document.getElementById("emailMsg").addEventListener("mouseover", emailAlert);
+
+/* Displays alert to user when they click to put in their email address */
+document.getElementById("emailMsg").addEventListener("click", emailAlert);
 
 function emailAlert() {
     alert("We promise to only send you helpful and relevant information.")
 }
 
-document.getElementById("myBtn").addEventListener("click", function() {
-    alert("Hello World!");
-  });
+document.getElementById("questionBox").addEventListener("click", userQInput);
+
+function userQInput() {
+  document.getElementById("tests").innerHTML = "Thank you for signing up! :)";
+}
