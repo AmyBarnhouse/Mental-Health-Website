0ec38ca55775b67d21eafcb0c863551ae19df128
AmyBarnhouse <107323584+AmyBarnhouse@users.noreply.github.com>
2022-11-18 14:58:48 +0000

    mouseover alert when user goes to put their email in

--- javaScript.js
+++ javaScript.js
@@ -4,3 +4,12 @@ function thankYouMsg() {
   document.getElementById("tests").innerHTML = "Thank you for signing up! :)";
 }
 
+document.getElementById("emailMsg").addEventListener("mouseover", emailAlert);
+
+function emailAlert() {
+    alert("We promise to only send you helpful and relevant information.")
+}
+
+document.getElementById("myBtn").addEventListener("click", function() {
+    alert("Hello World!");
+  });
