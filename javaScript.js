44b1454175371155c97343a34f97738f011e6c9b
AmyBarnhouse <107323584+AmyBarnhouse@users.noreply.github.com>
2022-11-21 19:15:06 +0000

    addition of 'all' data option and edits to dataset

--- javaScript.js
+++ javaScript.js
@@ -20,7 +20,7 @@ function userQInput() {
     document.getElementById("qBox").innerHTML;
 }
 
-// create 2 data_set
+/* Adapted from https://d3-graph-gallery.com/graph/barplot_button_data_hard.html */
 var data1 = [
   {group: "6 - 10 years old", value: 25},
   {group: "11 - 16 years old", value: 75},
@@ -33,12 +33,13 @@ var data2 = [
   {group: "17 - 23 years old", value: 95},
 ];
 
-// set the dimensions and margins of the graph
+var data3 = 
+
 var margin = {top: 30, right: 30, bottom: 70, left: 60},
    width = 460 - margin.left - margin.right,
    height = 400 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
+
 var svg = d3.select("#barChart")
  .append("svg")
    .attr("width", width + margin.left + margin.right)
@@ -47,32 +48,32 @@ var svg = d3.select("#barChart")
    .attr("transform",
          "translate(" + margin.left + "," + margin.top + ")");
 
-// Initialize the X axis
+
 var x = d3.scaleBand()
  .range([ 0, width ])
  .padding(0.2);
 var xAxis = svg.append("g")
  .attr("transform", "translate(0," + height + ")")
 
-// Initialize the Y axis
+
 var y = d3.scaleLinear()
  .range([ height, 0]);
 var yAxis = svg.append("g")
  .attr("class", "myYaxis")
 
 
-// A function that create / update the plot for a given variable:
+
 function update(data) {
 
- // Update the X axis
+
  x.domain(data.map(function(d) { return d.group; }))
  xAxis.call(d3.axisBottom(x))
 
- // Update the Y axis
+
  y.domain([0, d3.max(data, function(d) { return d.value }) ]);
  yAxis.transition().duration(1000).call(d3.axisLeft(y));
 
- // Create the u variable
+
  var u = svg.selectAll("rect")
    .data(data)
 
@@ -94,5 +95,4 @@ function update(data) {
    .remove()
 }
 
-// Initialize the plot with the first dataset
 update(data1)
