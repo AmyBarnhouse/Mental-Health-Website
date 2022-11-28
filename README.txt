b0b7d36d959994a3ccc997eae5aaacf6d1582eba
AmyBarnhouse <107323584+AmyBarnhouse@users.noreply.github.com>
2022-11-28 22:06:54 +0000

    references

--- README.txt
+++ README.txt
@@ -1,7 +1,17 @@
-Summative Assignment: Mental Health and Wellbeing WebsiteFor my assignment, I have created a website using HTML, CSS and JavaScript, aimed at students and young people who are seeking advice regarding their mental health.In order for the d3 visualisations to be properly visible, the website needs to be run on a local server. This to avoid CORS errors when my .csv dataset file is imported.
+Summative Assignment: Mental Health and Wellbeing Website
+Github:https://github.com/AmyBarnhouse/Mental-Health-Website
 
-Reference:
--
+For my assignment, I have created a website using HTML, CSS and JavaScript, aimed at students and young people who are seeking advice regarding their mental health.
+
+In order for the d3 visualisations to be properly visible, the website needs to be run on a local server. This to avoid CORS errors when my .csv dataset file is imported.
+
+References:
+Bootstrap(2022) 'Checks and radios'. Available at: https://github.com/twbs/bootstrap/blob/v5.2.2/site/content/docs/5.2/forms/checks-radios.md (Accessed: 14.11.2022).
+Boostrap (2022) 'Left-aligned nav'. Available at: https://getbootstrap.com/docs/5.2/components/navs-tabs/ (Accessed: 2.11.2022).
+Mind(2017) 'Mental health problems - an introduction'. Available at: https://www.mind.org.uk/information-support/types-of-mental-health-problems/mental-health-problems-introduction/causes/ (Accessed: 2.11.2022)
+freeCodeCamp (2022) '(New) Responsive Web Design'. Available at: https://www.freecodecamp.org/learn/2022/responsive-web-design/ (Accessed: 2.11.2022)
+Dönmez, B. Stone stack [Photograph]. Available at: https://unsplash.com/photos/eofm5R5f9Kw (Accessed: 2.11.2022)
+Bootstrap 'Heroes'. Available at: https://getbootstrap.com/docs/5.2/examples/heroes/ (Accessed: 2.11.2022)
 
 
 
--- script.js
+++ script.js
@@ -6,91 +6,10 @@ function thankYouMsg () {
 }
 
 document.getElementById('emailMsg').addEventListener('click', emailAlert)
-
 /** Displays alert to user when they click to put in their email address. */
 function emailAlert () {
   alert('We promise to only send you helpful and relevant information.')
 }
-
-/**  Adapted from https://d3-graph-gallery.com/graph/barplot_button_data_hard.html */
-// var data1 = [
-//   {group: "6 - 10 years old", value: 29},
-//   {group: "11 - 16 years old", value: 18},
-//   {group: "17 - 23 years old", value: 39}
-// ];
-
-// var data2 = [
-//   {group: "6 - 10 years old", value: 31},
-//   {group: "11 - 16 years old", value: 28},
-// ];
-
-// var data3 = [
-//   {group: "6 - 10 years old", value: 30},
-//   {group: "11 - 16 years old", value: 23},
-//   {group: "17 - 23 years old", value: 42}
-// ]
-
-// var margin = {top: 30, right: 30, bottom: 70, left: 60},
-//    width = 460 - margin.left - margin.right,
-//    height = 400 - margin.top - margin.bottom;
-
-// var svg = d3.select("#barChart")
-//  .append("svg")
-//    .attr("width", width + margin.left + margin.right)
-//    .attr("height", height + margin.top + margin.bottom)
-//  .append("g")
-//    .attr("transform",
-//          "translate(" + margin.left + "," + margin.top + ")");
-
-// var x = d3.scaleBand()
-//  .range([ 0, width ])
-//  .padding(0.2);
-// var xAxis = svg.append("g")
-//  .attr("transform", "translate(0," + height + ")")
-
-// var y = d3.scaleLinear()
-//  .range([ height, 0]);
-// var yAxis = svg.append("g")
-//  .attr("class", "myYaxis")
-
-// function update(data) {
-
-//  x.domain(data.map(function(d) { return d.group; }))
-//  xAxis.call(d3.axisBottom(x))
-
-//  y.domain([0, d3.max(data, function(d) { return d.value }) ]);
-//  yAxis.transition().duration(1000).call(d3.axisLeft(y));
-
-//  var u = svg.selectAll("rect")
-//    .data(data)
-
-//  u
-//    .enter()
-//    .append("rect") // Add a new rect for each new elements
-//    .merge(u) // get the already existing elements as well
-//    .transition() // and apply changes to all of them
-//    .duration(1000)
-//      .attr("x", function(d) { return x(d.group); })
-//      .attr("y", function(d) { return y(d.value); })
-//      .attr("width", x.bandwidth())
-//      .attr("height", function(d) { return height - y(d.value); })
-//      .attr("fill", "#69b3a2")
-
-//  // If less group in the new dataset, I delete the ones not in use anymore
-//  u
-//    .exit()
-//    .remove()
-// }
-
-// update(data1)
-
-// document.getElementById("stone-picture").onmouseover = function() {
-//   mouseOver()
-// };
-// function mouseOver() {
-//   var img = document.querySelector("#stone-picture");
-//   img.height = 100;
-// }
 
 // set the dimensions and margins of the graph
 const margin = { top: 30, right: 30, bottom: 70, left: 60 }
@@ -119,8 +38,7 @@ const y = d3.scaleLinear()
 const yAxis = svg.append('g')
   .attr('class', 'myYaxis')
 
-
-// A function that create / update the plot for a given variable:
+/** This function imports the dataset and presents it in a bar chart.  */
 function update (selectedVar) {
   // Parse the Data
   d3.csv('dataset_final.csv', function (data) {
@@ -147,19 +65,6 @@ function update (selectedVar) {
       .attr('width', x.bandwidth())
       .attr('height', function (d) { return height - y(d[selectedVar]) })
       .attr('fill', '#69b3a2')
-      // .on("mouseover", function(d, i) {
-      //   tooltip.html(`Data: ${d}`).style("visibility", "visible");
-      //   d3.select(this)
-      //     .attr("fill", shadeColor(bar_color, -15));
-      // })
-      // .on("mousemover", function(){
-      //   tooltip
-      //     .style("top", (event.pageY-10)+"px")
-      //     .style("left",(event.pageX+10)+"px");
-      // })
-      // .on("mouseout", function() {
-      //   tooltip.html(``).style("visibility", "hidden");
-      //   d3.select(this).attr("fill", bar_color);
   })
 }
 
