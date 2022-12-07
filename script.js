c66aba41b8964f9838352aedb7a259403095aad2
AmyBarnhouse <107323584+AmyBarnhouse@users.noreply.github.com>
2022-12-07 17:10:35 +0000

    snackbar added, scatter line graph added

--- script.js
+++ script.js
@@ -24,12 +24,6 @@ document.getElementById('colourBackTrend').addEventListener('mouseout', function
   document.getElementById('colourBackTrend').style.backgroundColor = '#fff'
 })
 
-document.getElementById('submitBtn').addEventListener('click', thankYouMsg)
-/** Function produces a 'thank you' message in response to the user clicking Submit after filling out their contact details. */
-function thankYouMsg () {
-  document.getElementById('tests').innerHTML = 'Thank you for signing up! :)'
-}
-
 document.getElementById('emailMsg').addEventListener('click', emailAlert)
 /** Displays alert to user when they click to put in their email address. */
 function emailAlert () {
@@ -37,23 +31,23 @@ function emailAlert () {
 }
 
 // set the dimensions and margins of the graph
-/* const margin = { top: 10, right: 100, bottom: 30, left: 30 }
-const width = 460 - margin.left - margin.right
-const height = 400 - margin.top - margin.bottom
+const margin1 = { top: 30, right: 300, bottom: 30, left: 30 }
+const width1 = 500 - margin1.left - margin1.right
+const height1 = 400 - margin1.top - margin1.bottom
 
 // append the svg object to the body of the page
-const svg = d3.select('#my_dataviz')
+const svg1 = d3.select('#my_dataviz')
   .append('svg')
-  .attr('width', width + margin.left + margin.right)
-  .attr('height', height + margin.top + margin.bottom)
+  .attr('width', width1 + margin1.left + margin1.right)
+  .attr('height', height1 + margin1.top + margin1.bottom)
   .append('g')
   .attr('transform',
-    'translate(' + margin.left + ',' + margin.top + ')')
+    'translate(' + margin1.left + ',' + margin1.top + ')')
 
 // Read the data
 d3.csv('scatter-dataset.csv', function (data1) {
   // List of groups (here I have one group per column)
-  const allGroup = ['Depression', 'Anxiety', 'PanicAttacks']
+  const allGroup = ['Panic', 'Anxiety', 'Depression']
 
   // Reformat the data: we need an array of arrays of {x, y} tuples
   const dataReady = allGroup.map(function (grpName) { // .map allows to do something for each element of the list
@@ -65,7 +59,7 @@ d3.csv('scatter-dataset.csv', function (data1) {
     }
   })
   // I strongly advise to have a look to dataReady with
-  // console.log(dataReady)
+  console.log(dataReady)
   // A color scale: one color for each group
   const myColor = d3.scaleOrdinal()
     .domain(allGroup)
@@ -75,7 +69,7 @@ d3.csv('scatter-dataset.csv', function (data1) {
   const x = d3.scaleLinear()
     .domain([18, 24])
     .range([0, width])
-  svg.append('g')
+  svg1.append('g')
     .attr('transform', 'translate(0,' + height + ')')
     .call(d3.axisBottom(x))
 
@@ -83,13 +77,13 @@ d3.csv('scatter-dataset.csv', function (data1) {
   const y = d3.scaleLinear()
     .domain([0, 100])
     .range([height, 0])
-  svg.append('g')
+  svg1.append('g')
     .call(d3.axisLeft(y))
   // Add the lines
   const line = d3.line()
     .x(function (d) { return x(+d.Age) })
     .y(function (d) { return y(+d.value) })
-  svg.selectAll('myLines')
+  svg1.selectAll('myLines')
     .data(dataReady)
     .enter()
     .append('path')
@@ -99,7 +93,7 @@ d3.csv('scatter-dataset.csv', function (data1) {
     .style('stroke-width', 4)
     .style('fill', 'none')
   // Add the points
-  svg
+  svg1
   // First we need to enter in a group
     .selectAll('myDots')
     .data(dataReady)
@@ -117,7 +111,7 @@ d3.csv('scatter-dataset.csv', function (data1) {
     .attr('r', 5)
     .attr('stroke', 'white')
   // Add a label at the end of each line
-  svg
+  svg1
     .selectAll('myLabels')
     .data(dataReady)
     .enter()
@@ -131,7 +125,7 @@ d3.csv('scatter-dataset.csv', function (data1) {
     .style('fill', function (d) { return myColor(d.name) })
     .style('font-size', 15)
   // Add a legend (interactive)
-  svg
+  svg1
     .selectAll('myLegend')
     .data(dataReady)
     .enter()
@@ -148,7 +142,7 @@ d3.csv('scatter-dataset.csv', function (data1) {
       // Change the opacity: from 0 to 1 or from 1 to 0
       d3.selectAll('.' + d.name).transition().style('opacity', currentOpacity == 1 ? 0 : 1)
     })
-}) */
+}) 
 
  // set the dimensions and margins of the graph
 const margin = { top: 30, right: 30, bottom: 70, left: 60 }
@@ -209,3 +203,14 @@ const yAxis = svg.append('g')
 
 // Initialize plot
 update('var1') 
+
+function popUp() {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
