83240bee301444ebd4a0e9f55b40fef935245dcd
AmyBarnhouse <107323584+AmyBarnhouse@users.noreply.github.com>
2022-12-07 16:26:43 +0000

    new dataset, working on new visualisation

--- script.js
+++ script.js
@@ -12,6 +12,18 @@ document.getElementById('colourBackTrend').addEventListener('mouseover', functio
   document.getElementById('colourBackTrend').style.backgroundColor = '#f8ebfa'
 })
 
+document.getElementById('colourBackStigma').addEventListener('mouseout', function () {
+  document.getElementById('colourBackStigma').style.backgroundColor = '#fff'
+})
+
+document.getElementById('colourBackIntro').addEventListener('mouseout', function () {
+  document.getElementById('colourBackIntro').style.backgroundColor = '#fff'
+})
+
+document.getElementById('colourBackTrend').addEventListener('mouseout', function () {
+  document.getElementById('colourBackTrend').style.backgroundColor = '#fff'
+})
+
 document.getElementById('submitBtn').addEventListener('click', thankYouMsg)
 /** Function produces a 'thank you' message in response to the user clicking Submit after filling out their contact details. */
 function thankYouMsg () {
@@ -25,6 +37,120 @@ function emailAlert () {
 }
 
 // set the dimensions and margins of the graph
+/* const margin = { top: 10, right: 100, bottom: 30, left: 30 }
+const width = 460 - margin.left - margin.right
+const height = 400 - margin.top - margin.bottom
+
+// append the svg object to the body of the page
+const svg = d3.select('#my_dataviz')
+  .append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+  .attr('transform',
+    'translate(' + margin.left + ',' + margin.top + ')')
+
+// Read the data
+d3.csv('scatter-dataset.csv', function (data1) {
+  // List of groups (here I have one group per column)
+  const allGroup = ['Depression', 'Anxiety', 'PanicAttacks']
+
+  // Reformat the data: we need an array of arrays of {x, y} tuples
+  const dataReady = allGroup.map(function (grpName) { // .map allows to do something for each element of the list
+    return {
+      name: grpName,
+      values: data1.map(function (d) {
+        return { Age: d.Age, value: +d[grpName] }
+      })
+    }
+  })
+  // I strongly advise to have a look to dataReady with
+  // console.log(dataReady)
+  // A color scale: one color for each group
+  const myColor = d3.scaleOrdinal()
+    .domain(allGroup)
+    .range(d3.schemeSet2)
+
+  // Add X axis --> it is a date format
+  const x = d3.scaleLinear()
+    .domain([18, 24])
+    .range([0, width])
+  svg.append('g')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(d3.axisBottom(x))
+
+  // Add Y axis
+  const y = d3.scaleLinear()
+    .domain([0, 100])
+    .range([height, 0])
+  svg.append('g')
+    .call(d3.axisLeft(y))
+  // Add the lines
+  const line = d3.line()
+    .x(function (d) { return x(+d.Age) })
+    .y(function (d) { return y(+d.value) })
+  svg.selectAll('myLines')
+    .data(dataReady)
+    .enter()
+    .append('path')
+    .attr('class', function (d) { return d.name })
+    .attr('d', function (d) { return line(d.values) })
+    .attr('stroke', function (d) { return myColor(d.name) })
+    .style('stroke-width', 4)
+    .style('fill', 'none')
+  // Add the points
+  svg
+  // First we need to enter in a group
+    .selectAll('myDots')
+    .data(dataReady)
+    .enter()
+    .append('g')
+    .style('fill', function (d) { return myColor(d.name) })
+    .attr('class', function (d) { return d.name })
+  // Second we need to enter in the 'values' part of this group
+    .selectAll('myPoints')
+    .data(function (d) { return d.values })
+    .enter()
+    .append('circle')
+    .attr('cx', function (d) { return x(d.Age) })
+    .attr('cy', function (d) { return y(d.value) })
+    .attr('r', 5)
+    .attr('stroke', 'white')
+  // Add a label at the end of each line
+  svg
+    .selectAll('myLabels')
+    .data(dataReady)
+    .enter()
+    .append('g')
+    .append('text')
+    .attr('class', function (d) { return d.name })
+    .datum(function (d) { return { name: d.name, value: d.values[d.values.length - 1] } }) // keep only the last value of each time series
+    .attr('transform', function (d) { return 'translate(' + x(d.value.Age) + ',' + y(d.value.value) + ')' }) // Put the text at the position of the last point
+    .attr('x', 12) // shift the text a bit more right
+    .text(function (d) { return d.name })
+    .style('fill', function (d) { return myColor(d.name) })
+    .style('font-size', 15)
+  // Add a legend (interactive)
+  svg
+    .selectAll('myLegend')
+    .data(dataReady)
+    .enter()
+    .append('g')
+    .append('text')
+    .attr('x', function (d, i) { return 30 + i * 60 })
+    .attr('y', 30)
+    .text(function (d) { return d.name })
+    .style('fill', function (d) { return myColor(d.name) })
+    .style('font-size', 15)
+    .on('click', function (d) {
+      // is the element currently visible ?
+      currentOpacity = d3.selectAll('.' + d.name).style('opacity')
+      // Change the opacity: from 0 to 1 or from 1 to 0
+      d3.selectAll('.' + d.name).transition().style('opacity', currentOpacity == 1 ? 0 : 1)
+    })
+}) */
+
+ // set the dimensions and margins of the graph
 const margin = { top: 30, right: 30, bottom: 70, left: 60 }
 const width = 460 - margin.left - margin.right
 const height = 400 - margin.top - margin.bottom
@@ -52,7 +178,7 @@ const yAxis = svg.append('g')
   .attr('class', 'myYaxis')
 
 /** This function imports the dataset and presents it in a bar chart.  */
-function update (selectedVar) {
+ function update (selectedVar) {
   // Parse the Data
   d3.csv('dataset_final.csv', function (data) {
     // X axis
@@ -82,4 +208,4 @@ function update (selectedVar) {
 }
 
 // Initialize plot
-update('var1')
+update('var1') 
